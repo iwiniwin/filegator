fa98e5766835d1de14edefb0fd6f2ab4f0c8239b
iwiniwin <iwiniwin@foxmail.com>
2022-06-21 21:20:03 +0800

    文本编辑器添加复制按钮

--- frontend/views/partials/Editor.vue
+++ frontend/views/partials/Editor.vue
@@ -12,6 +12,9 @@
         </template>
       </section>
       <footer class="modal-card-foot">
+        <button class="button" type="button" @click="copyContent()">
+          {{ lang('Copy') }}
+        </button>
         <button v-if="can('write')" class="button" type="button" @click="saveFile()">
           {{ lang('Save') }}
         </button>
@@ -65,6 +68,23 @@ export default {
           this.$parent.close()
         })
         .catch(error => this.handleError(error))
+    },
+    copyContent() {
+        const textarea = document.createElement('textarea')
+        // 将该 textarea 设为 readonly 防止 iOS 下自动唤起键盘，同时将 textarea 移出可视区域
+        textarea.readOnly = 'readonly'
+        textarea.style.position = 'absolute'
+        textarea.style.left = '-9999px'
+        // 将要 copy 的值赋给 textarea 标签的 value 属性  
+        // 网上有些例子是赋值给innerText,这样也会赋值成功，但是识别不了\r\n的换行符，赋值给value属性就可以
+        textarea.value = this.content
+        // 将 textarea 插入到 body 中
+        document.body.appendChild(textarea)
+        // 选中值并复制
+        textarea.select()
+        textarea.setSelectionRange(0, textarea.value.length)
+        document.execCommand('Copy')
+        document.body.removeChild(textarea)
     }
   },
 }
